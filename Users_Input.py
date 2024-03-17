d6b6e6bbd42aa3966bdc7441fb6cbcc9cac7c089
John <gadjohn582@gmail.com>
2024-03-17 16:29:31 +0300

    First commit

---
+++ Users_Input.py
@@ -0,0 +1,13 @@
+name = input("Enter your name: ")
+# asks the user for their name
+
+
+age = input("Enter your age: ")
+# asks the user for their age
+
+
+location = input("Enter your location: ")
+# asks the user for their location
+
+# Prints out a personalized message
+print(f"Hello {name}, you are {age} years old and live in {location}.")
